1d1378382af79c13034a291a059c4c585965e609
Valentin Voigt <mail@valentinvoigt.info>
2024-10-18 23:18:13 +0200

    feat(nvim): add rust-tools

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -517,4 +517,18 @@ require'marks'.setup {
   builtin_marks = { "." },
 }
 
+-- rust
+local rt = require("rust-tools")
+
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
+
 -- vim: ts=2 sts=2 sw=2 et
