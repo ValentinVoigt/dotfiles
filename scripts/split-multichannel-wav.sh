2f9cc318016d610467898a612e6ea4ae83397d48
Valentin Voigt <mail@valentinvoigt.info>
2024-12-08 20:30:43 +0100

    feat(scripts): improve split-multichannel-wav

--- scripts/split-multichannel-wav.sh
+++ scripts/split-multichannel-wav.sh
@@ -2,6 +2,11 @@
 
 set -eu
 
+_term() {
+	echo "Caught SIGTERM, killing children :O"
+	pkill -P $$
+}
+
 if [ "$#" -lt 1 ]; then
 	echo "Usage: $0 <filename-of-multichannel-wav>" >&2
 	echo "" >&2
@@ -14,8 +19,16 @@ elif [ ! -r "$1" ]; then
 	echo "Error: File is not readable: $1" >&2
 	exit 2
 else
+	trap _term SIGTERM
+	trap _term SIGINT
+
 	for i in $(seq 1 32); do
 		cmd="sox"
+
+		if [ $i == 1 ]; then
+			cmd="$cmd -S"
+		fi
+
 		for file in "$@"; do
 			cmd="$cmd \"$file\""
 		done
